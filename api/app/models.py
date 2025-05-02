f9dbece551f3116c22d3c42aad2b1957fbde9d6c
Christopher Bonner <cbonner.dev@outlook.com>
2025-05-02 23:36:38 +0100

    Hashtags and videos

--- api/app/models.py
+++ api/app/models.py
@@ -12,11 +12,13 @@ def get_uuid():
     return uuid4().hex
 
 
-class User(UserMixin, db.Model): 
+class User(UserMixin, db.Model):
     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid) # default=get_uuid ensures each user is uuid not normal autonumber 
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(300), nullable=False)
     password = db.Column(db.Text)
+
+    videos = db.relationship("Video", back_populates="user", cascade="all, delete")
 
     # flask login NEEDS this to be implemented
     def get_id(self):
@@ -32,3 +34,33 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         hashed_password = self.password.encode("utf-8")
         return bcrypt.checkpw(password.encode(), hashed_password)
+
+
+# many to many
+video_hashtags = db.Table(
+    "video_hashtags",
+    db.Column("video_id", db.String(32), db.ForeignKey("video.id")),
+    db.Column("hashtag_id", db.String(32), db.ForeignKey("hashtag.id")),
+)
+
+
+class Video(db.Model):
+    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
+    caption = db.Column(db.String(100), nullable=True)
+    user_id = db.Column(db.String(32), db.ForeignKey("user.id"), nullable=False)
+
+    user = db.relationship("User", back_populates="videos", cascade="all, delete")
+    hashtags = db.relationship("Hashtag", secondary=video_hashtags, back_populates="videos")
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "caption": self.caption
+        }
+
+
+class Hashtag(db.Model):
+    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
+    name = db.Column(db.String(30), nullable=False)
+
+    videos = db.relationship("Video", secondary=video_hashtags, back_populates="hashtags")
--- api/app/routes/video.py
+++ api/app/routes/video.py
@@ -8,10 +8,16 @@ video = Blueprint("video", __name__)
 
 @video.route("/video/<key>")
 def get_video_url(key):
+    # expiry time
     expires_at = datetime.now() + timedelta(hours=1)
+
+    # pass in private key secret name and public key id
     cfu = CloudFrontUtil("cf-private-1", "K3LWK7UJYZTKK1")
+
+    # the video in cloudfront
     url = f"https://d35x6vjrwluk3o.cloudfront.net/{key}"
 
+    # the signed url
     signed_url = cfu.generate_presigned_url(url, expires_at)
 
     return jsonify({"url": signed_url})
