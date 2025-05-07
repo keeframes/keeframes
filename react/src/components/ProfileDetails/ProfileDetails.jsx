11f38f24ed0eb67e92e6839c9ad59e5ef67ab4d5
Christopher Bonner <cbonner.dev@outlook.com>
2025-05-07 05:22:02 +0100

    added some comments

--- react/src/components/ProfileDetails/ProfileDetails.jsx
+++ react/src/components/ProfileDetails/ProfileDetails.jsx
@@ -3,10 +3,9 @@ import { CDN_URL } from "../../utils/constants";
 import Count from "../../components/Count/Count";
 import { followUser, unfollowUser } from "../../api/user";
 import { useParams } from "react-router-dom";
-import { createContext, use } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-export default function ProfileDetails({ profile, setProfile, children }) {
+export default function ProfileDetails({ profile, children }) {
   return (
     <div className={styles.profile}>
       <div className={styles.details}>
@@ -35,13 +34,16 @@ export default function ProfileDetails({ profile, setProfile, children }) {
   );
 }
 
+// The follow button
 ProfileDetails.FollowButton = function FollowButton() {
   const { username } = useParams();
   const queryClient = useQueryClient();
 
+  // update the cache for when the user follows someone
   const followMutation = useMutation({
     mutationFn: () => followUser(username),
     onSuccess: () => {
+      // change the cache at that key
       queryClient.setQueryData(["profile", username], (old) => {
         if (!old) return old;
 
@@ -65,17 +67,21 @@ ProfileDetails.FollowButton = function FollowButton() {
   );
 };
 
+// the unfollow button
 ProfileDetails.UnfollowButton = function UnfollowButton() {
   const { username } = useParams();
   const queryClient = useQueryClient();
 
+  // run the mutation when the button is pressed
   const handleClick = (e) => {
     unFollowMutation.mutate();
   };
 
+  // update the cache for when the user unfollows someone
   const unFollowMutation = useMutation({
     mutationFn: () => unfollowUser(username),
     onSuccess: () => {
+      // change the cache at the specified key
       queryClient.setQueryData(["profile", username], (old) => {
         if (!old) return old;
 
@@ -95,10 +101,12 @@ ProfileDetails.UnfollowButton = function UnfollowButton() {
   );
 };
 
+// message button
 ProfileDetails.MessageButton = function MessageButton() {
   return <button className={styles.message}>Message</button>;
 };
 
+// edit button
 ProfileDetails.EditButton = function EditButton() {
   return <button className={styles.edit}>Edit Profile</button>;
 };
